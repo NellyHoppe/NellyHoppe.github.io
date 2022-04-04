67949570b380e8badc174182cc9ed597522e3cd2
Nelly Hoppe <nelly.hoppe@student.uibk.ac.at>
2022-04-04 15:43:17 +0200

    5/10

--- codecademy/js_introduction/app.js
+++ codecademy/js_introduction/app.js
@@ -1,4 +1,4 @@
-console.log('JavaScript')
-console.log(2011)
-console.log('Woohoo! I love to code! #codecademy')
-console.log(20.49)
+onsole.log(27+3.5)
+console.log(2022-1969)
+console.log(65/240)
+console.log(0.2708*100)
