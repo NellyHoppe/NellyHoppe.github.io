0e201391c4d6f4d0a23938ab8c05a606433e5746
Nelly Hoppe <nelly.hoppe@student.uibk.ac.at>
2022-04-07 15:57:19 +0200

    array coords erstellt und abgerufen

--- nz/main.js
+++ nz/main.js
@@ -5,12 +5,20 @@ let lat = -36.833333
 let lng = 174.8
 let zoom = 10
 
-let map = L.map('map').setView([lat, lng], zoom);
+let coords = [-36.833333, 174.8]
+console.log(coords);
+console.log(coords[0]);
+console.log(coords[1]);
+console.log(coords.length);
+
+
+
+let map = L.map('map').setView(coords, zoom);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker([lat, lng]).addTo(map)
+        L.marker(coords).addTo(map)
             .bindPopup('<h3>Auckland</h3>')
             .openPopup();
