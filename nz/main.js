1e713b74ab6e35a1b4ffe272505cc59ce82a4b4f
Nelly Hoppe <nelly.hoppe@student.uibk.ac.at>
2022-04-07 15:44:25 +0200

    Variablen für Koordinaten

--- nz/main.js
+++ nz/main.js
@@ -1,12 +1,16 @@
 /*Neuseelandreise Skript*/
 // einzeiliger Komentar
 
-var map = L.map('map').setView([-36.833333, 174.8], 10);
+let lat = -36.833333
+let lng = 174.8
+let zoom = 10
+
+let map = L.map('map').setView([lat, lng], zoom);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker([-36.833333, 174.8]).addTo(map)
+        L.marker([lat, lng]).addTo(map)
             .bindPopup('<h3>Auckland</h3>')
             .openPopup();
