8a0816242239893028ba25a4ad5f58069b68e546
Nelly Hoppe <nelly.hoppe@student.uibk.ac.at>
2022-04-28 15:51:17 +0200

    eigenes Popup in if-Abfrage öffnen

--- nz/main.js
+++ nz/main.js
@@ -5,7 +5,10 @@ let lat = -36.833333
 let lng = 174.8
 let zoom = 10
 
-let coords = [-36.833333, 174.8]
+let coords = [
+    ETAPPEN[18].lat,
+    ETAPPEN[18].lng
+];
 //console.log(coords);
 //console.log(coords[0]);
 //console.log(coords[1]);
@@ -16,6 +19,8 @@ let coords = [-36.833333, 174.8]
 //console.log('id="map"');
 //console.log(`latitude = ${lat}`)
 
+let map = L.map('map').setView(coords, zoom);
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
@@ -24,14 +29,16 @@ for (let etappe of ETAPPEN) {
     let popup = `
     <h3>${etappe.titel} (Etappe ${etappe.nr})</h3>
     <ul>
-    <li>geogr. Länge: ${etappe.lng}</li> 
-    <li>geogr. Breite: ${etappe.lat}</li>
-    <li><a href="${etappe.wikipedia}">Link zur Wikipediaseite</a></li>
-    <li><a href="https://${etappe.github}.github.io/nz/index.html">Link zur Etappenseite</a></li>
+        <li>geogr. Länge: ${etappe.lng}</li> 
+        <li>geogr. Breite: ${etappe.lat}</li>
+        <li><a href="${etappe.wikipedia}">Link zur Wikipediaseite</a></li>
+        <li><a href="https://${etappe.github}.github.io/nz/index.html">Link zur Etappenseite</a></li>
     </ul>`
     //console.log(etappe)
-    L.marker([etappe.lat, etappe.lng]).addTo(map)
-    .bindPopup(popup)
+    let mrk = L.marker([etappe.lat, etappe.lng]).addTo(map).bindPopup(popup)
+    if (etappe.nr == 19){
+        mrk.openPopup();
+    }
 
     //Etappennavigation erweitern
     let link = `<a href="https://${etappe.github}.github.io/nz/index.html" class="etappenLink" title="${etappe.titel}">${etappe.nr}</a>`;
@@ -39,7 +46,7 @@ for (let etappe of ETAPPEN) {
 }
 
 //https://webmapping.github.io/nz/huts.js
-for (let hut of HUTS){
+for (let hut of HUTS) {
     let popup = `
     <h3>${hut.name}</h3>
     <h4>${hut.region}</h3>
@@ -50,5 +57,5 @@ for (let hut of HUTS){
     <a href="${hut.link}" target="Neuseeland">Link zur Hütte</a>
     `
     L.circleMarker([hut.lat, hut.lng]).addTo(map)
-    .bindPopup(popup)
+        .bindPopup(popup)
 }
